45c2992750aa367c8ec7e497bbcac2d9a91be4d1
fylzzz <ichocolqtemilk@outlook.com>
2026-06-23 15:16:37 +1000

    updated to separate engine from game builds

---
+++ Game/build/main.d
@@ -0,0 +1,168 @@
+main.o: \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/src/main.cpp \
+ C:/devkitPro/libogc2/wii/include/raylib.h \
+ C:/devkitPro/libogc2/wii/include/gccore.h \
+ C:/devkitPro/libogc2/wii/include/ogc/dsp.h \
+ C:/devkitPro/libogc2/wii/include/gctypes.h \
+ C:/devkitPro/libogc2/wii/include/ogc/aram.h \
+ C:/devkitPro/libogc2/wii/include/ogc/disc_io.h \
+ C:/devkitPro/libogc2/wii/include/ogc/arqueue.h \
+ C:/devkitPro/libogc2/wii/include/ogc/lwp_queue.h \
+ C:/devkitPro/libogc2/wii/include/ogc/aram.h \
+ C:/devkitPro/libogc2/wii/include/ogc/arqmgr.h \
+ C:/devkitPro/libogc2/wii/include/ogc/audio.h \
+ C:/devkitPro/libogc2/wii/include/ogc/cache.h \
+ C:/devkitPro/libogc2/wii/include/ogc/card.h \
+ C:/devkitPro/libogc2/wii/include/ogc/cast.h \
+ C:/devkitPro/libogc2/wii/include/ogc/color.h \
+ C:/devkitPro/libogc2/wii/include/ogc/consol.h \
+ C:/devkitPro/libogc2/wii/include/ogc/gx_struct.h \
+ C:/devkitPro/libogc2/wii/include/ogc/dvd.h \
+ C:/devkitPro/libogc2/wii/include/ogc/exi.h \
+ C:/devkitPro/libogc2/wii/include/ogc/gu.h \
+ C:/devkitPro/libogc2/wii/include/ogc/gx.h \
+ C:/devkitPro/libogc2/wii/include/ogc/lwp.h \
+ C:/devkitPro/libogc2/wii/include/ogc/gu.h \
+ C:/devkitPro/libogc2/wii/include/ogc/si.h \
+ C:/devkitPro/libogc2/wii/include/ogc/gx_struct.h \
+ C:/devkitPro/libogc2/wii/include/ogc/irq.h \
+ C:/devkitPro/libogc2/wii/include/ogc/context.h \
+ C:/devkitPro/libogc2/wii/include/ogc/lwp.h \
+ C:/devkitPro/libogc2/wii/include/ogc/mutex.h \
+ C:/devkitPro/libogc2/wii/include/ogc/message.h \
+ C:/devkitPro/libogc2/wii/include/ogc/semaphore.h \
+ C:/devkitPro/libogc2/wii/include/ogc/pad.h \
+ C:/devkitPro/libogc2/wii/include/ogc/tpl.h \
+ C:/devkitPro/libogc2/wii/include/ogc/gx.h \
+ C:/devkitPro/libogc2/wii/include/ogc/system.h \
+ C:/devkitPro/libogc2/wii/include/gcutil.h \
+ C:/devkitPro/libogc2/wii/include/ogc/video.h \
+ C:/devkitPro/libogc2/wii/include/ogc/video_types.h \
+ C:/devkitPro/libogc2/wii/include/ogc/usbgecko.h \
+ C:/devkitPro/libogc2/wii/include/ogc/video_types.h \
+ C:/devkitPro/libogc2/wii/include/ogc/texconv.h \
+ C:/devkitPro/libogc2/wii/include/ogc/ipc.h \
+ C:/devkitPro/libogc2/wii/include/ogc/es.h \
+ C:/devkitPro/libogc2/wii/include/ogc/stm.h \
+ C:/devkitPro/libogc2/wii/include/ogc/ios.h \
+ C:/devkitPro/libogc2/wii/include/ogc/usb.h \
+ C:/devkitPro/libogc2/wii/include/ogc/isfs.h \
+ C:/devkitPro/libogc2/wii/include/ogc/conf.h \
+ C:/devkitPro/libogc2/wii/include/ogc/usbstorage.h \
+ C:/devkitPro/libogc2/wii/include/ogc/mutex.h \
+ C:/devkitPro/libogc2/wii/include/ogc/system.h \
+ C:/devkitPro/libogc2/wii/include/ogc/wiilaunch.h \
+ C:/devkitPro/libogc2/wii/include/GL/gl.h \
+ C:/devkitPro/libogc2/wii/include/GL/glext.h \
+ C:/devkitPro/libogc2/wii/include/KHR/khrplatform.h \
+ C:/devkitPro/libogc2/wii/include/fat.h \
+ C:/devkitPro/libogc2/wii/include/dvm.h \
+ C:/devkitPro/libogc2/wii/include/wiiuse/wpad.h \
+ C:/devkitPro/libogc2/wii/include/wiiuse/wiiuse.h \
+ C:/devkitPro/libogc2/wii/include/bte/bte.h \
+ C:/devkitPro/libogc2/wii/include/bte/bd_addr.h \
+ C:/devkitPro/libogc2/wii/include/ogc/lwp_watchdog.h \
+ C:/devkitPro/libogc2/wii/include/ogc/lwp_queue.h \
+ C:/devkitPro/libogc2/wii/include/ogc/timesupp.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/World.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/EntityManager.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/Types.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/ComponentManager.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/ComponentArray.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/TypeId.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/SystemManager.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/System.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/ResourceManager.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/SpriteManager.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/AnimationManager.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/Serializer.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Components.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Systems/RenderSystem.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Systems/PhysicsSystem.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/SceneManager.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/Scene.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Game/Scenes/SampleScene.h \
+ C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Systems/AnimationSystem.h
+C:/devkitPro/libogc2/wii/include/raylib.h:
+C:/devkitPro/libogc2/wii/include/gccore.h:
+C:/devkitPro/libogc2/wii/include/ogc/dsp.h:
+C:/devkitPro/libogc2/wii/include/gctypes.h:
+C:/devkitPro/libogc2/wii/include/ogc/aram.h:
+C:/devkitPro/libogc2/wii/include/ogc/disc_io.h:
+C:/devkitPro/libogc2/wii/include/ogc/arqueue.h:
+C:/devkitPro/libogc2/wii/include/ogc/lwp_queue.h:
+C:/devkitPro/libogc2/wii/include/ogc/aram.h:
+C:/devkitPro/libogc2/wii/include/ogc/arqmgr.h:
+C:/devkitPro/libogc2/wii/include/ogc/audio.h:
+C:/devkitPro/libogc2/wii/include/ogc/cache.h:
+C:/devkitPro/libogc2/wii/include/ogc/card.h:
+C:/devkitPro/libogc2/wii/include/ogc/cast.h:
+C:/devkitPro/libogc2/wii/include/ogc/color.h:
+C:/devkitPro/libogc2/wii/include/ogc/consol.h:
+C:/devkitPro/libogc2/wii/include/ogc/gx_struct.h:
+C:/devkitPro/libogc2/wii/include/ogc/dvd.h:
+C:/devkitPro/libogc2/wii/include/ogc/exi.h:
+C:/devkitPro/libogc2/wii/include/ogc/gu.h:
+C:/devkitPro/libogc2/wii/include/ogc/gx.h:
+C:/devkitPro/libogc2/wii/include/ogc/lwp.h:
+C:/devkitPro/libogc2/wii/include/ogc/gu.h:
+C:/devkitPro/libogc2/wii/include/ogc/si.h:
+C:/devkitPro/libogc2/wii/include/ogc/gx_struct.h:
+C:/devkitPro/libogc2/wii/include/ogc/irq.h:
+C:/devkitPro/libogc2/wii/include/ogc/context.h:
+C:/devkitPro/libogc2/wii/include/ogc/lwp.h:
+C:/devkitPro/libogc2/wii/include/ogc/mutex.h:
+C:/devkitPro/libogc2/wii/include/ogc/message.h:
+C:/devkitPro/libogc2/wii/include/ogc/semaphore.h:
+C:/devkitPro/libogc2/wii/include/ogc/pad.h:
+C:/devkitPro/libogc2/wii/include/ogc/tpl.h:
+C:/devkitPro/libogc2/wii/include/ogc/gx.h:
+C:/devkitPro/libogc2/wii/include/ogc/system.h:
+C:/devkitPro/libogc2/wii/include/gcutil.h:
+C:/devkitPro/libogc2/wii/include/ogc/video.h:
+C:/devkitPro/libogc2/wii/include/ogc/video_types.h:
+C:/devkitPro/libogc2/wii/include/ogc/usbgecko.h:
+C:/devkitPro/libogc2/wii/include/ogc/video_types.h:
+C:/devkitPro/libogc2/wii/include/ogc/texconv.h:
+C:/devkitPro/libogc2/wii/include/ogc/ipc.h:
+C:/devkitPro/libogc2/wii/include/ogc/es.h:
+C:/devkitPro/libogc2/wii/include/ogc/stm.h:
+C:/devkitPro/libogc2/wii/include/ogc/ios.h:
+C:/devkitPro/libogc2/wii/include/ogc/usb.h:
+C:/devkitPro/libogc2/wii/include/ogc/isfs.h:
+C:/devkitPro/libogc2/wii/include/ogc/conf.h:
+C:/devkitPro/libogc2/wii/include/ogc/usbstorage.h:
+C:/devkitPro/libogc2/wii/include/ogc/mutex.h:
+C:/devkitPro/libogc2/wii/include/ogc/system.h:
+C:/devkitPro/libogc2/wii/include/ogc/wiilaunch.h:
+C:/devkitPro/libogc2/wii/include/GL/gl.h:
+C:/devkitPro/libogc2/wii/include/GL/glext.h:
+C:/devkitPro/libogc2/wii/include/KHR/khrplatform.h:
+C:/devkitPro/libogc2/wii/include/fat.h:
+C:/devkitPro/libogc2/wii/include/dvm.h:
+C:/devkitPro/libogc2/wii/include/wiiuse/wpad.h:
+C:/devkitPro/libogc2/wii/include/wiiuse/wiiuse.h:
+C:/devkitPro/libogc2/wii/include/bte/bte.h:
+C:/devkitPro/libogc2/wii/include/bte/bd_addr.h:
+C:/devkitPro/libogc2/wii/include/ogc/lwp_watchdog.h:
+C:/devkitPro/libogc2/wii/include/ogc/lwp_queue.h:
+C:/devkitPro/libogc2/wii/include/ogc/timesupp.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/World.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/EntityManager.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/Types.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/ComponentManager.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/ComponentArray.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/TypeId.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/SystemManager.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/System.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/ResourceManager.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/SpriteManager.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/AnimationManager.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/Serializer.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Components.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Systems/RenderSystem.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Systems/PhysicsSystem.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/SceneManager.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/ECS/Scene.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Game/Scenes/SampleScene.h:
+C:/Users/iChoc/OneDrive/Documents/GitHub/wiiEngine/Engine/include/Systems/AnimationSystem.h:
---
+++ Game/main.cpp
@@ -0,0 +1,114 @@
+#include <raylib.h>
+#include <gccore.h>
+#include "GL/gl.h"
+#include "fat.h"
+#include <math.h>
+
+#include <wiiuse/wpad.h>
+#include <ogc/lwp_watchdog.h>
+
+#include "World.h"
+#include "Components.h"
+#include "RenderSystem.h"
+#include "PhysicsSystem.h"
+#include "SceneManager.h"
+
+
+// Scenes to include
+#include "SampleScene.h"
+
+
+void SetDrawMode2D() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, 640, 480, 0, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	return;
+}
+
+void SetDrawMode3D(Camera3D* camera) {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	float aspect = 640.0f / 480.0f;
+	float near = 0.05f, far = 1000.0f;
+	float t = near * tanf(camera->fovy * 0.5f * 3.14159f / 180.0f);
+	glFrustum(-t * aspect, t * aspect, -t, t, near, far);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Manual lookAt
+	Vector3 eye = camera->position;
+	Vector3 center = camera->target;
+	Vector3 up = camera->up;
+
+	// forward = normalize(center - eye)
+	float fx = center.x - eye.x, fy = center.y - eye.y, fz = center.z - eye.z;
+	float fl = sqrtf(fx * fx + fy * fy + fz * fz);
+	fx /= fl; fy /= fl; fz /= fl;
+
+	// right = normalize(forward x up)
+	float rx = fy * up.z - fz * up.y;
+	float ry = fz * up.x - fx * up.z;
+	float rz = fx * up.y - fy * up.x;
+	float rl = sqrtf(rx * rx + ry * ry + rz * rz);
+	rx /= rl; ry /= rl; rz /= rl;
+
+	// up = right x forward
+	float ux = ry * fz - rz * fy;
+	float uy = rz * fx - rx * fz;
+	float uz = rx * fy - ry * fx;
+
+	float m[16] = {
+		rx, ux, -fx, 0,
+		ry, uy, -fy, 0,
+		rz, uz, -fz, 0,
+		-(rx * eye.x + ry * eye.y + rz * eye.z),
+		-(ux * eye.x + uy * eye.y + uz * eye.z),
+		(fx * eye.x + fy * eye.y + fz * eye.z), 1
+	};
+	glMultMatrixf(m);
+
+	glDisable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+}
+
+int main() {
+	fatInitDefault();
+
+	WPAD_Init();
+	expansion_t data;
+	WPAD_Expansion(WPAD_CHAN_ALL, &data);
+	WPAD_SetDataFormat(WPAD_CHAN_ALL, WPAD_FMT_BTNS_ACC_IR);
+
+	InitWindow(640, 480, "Wii Raylib");
+	//SetTargetFPS(60);
+
+	SceneManager scenes;
+	scenes.registerScene<SampleScene>(0);
+	scenes.switchTo(0);
+
+
+	while (!WindowShouldClose()) {
+		static uint64_t lastTime = gettime();
+		uint64_t now = gettime();
+		float dt = (float)diff_usec(lastTime, now) / 1000000.0f;
+		lastTime = now;
+		if (dt > 0.1f) dt = 0.1f;
+
+		WPAD_ScanPads();
+		if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME) break;
+
+		scenes.update(dt);
+		scenes.render(dt);
+		
+	}
+
+	if (scenes.current()) {
+		scenes.current()->shutdown();
+	}
+
+	CloseWindow();
+	return 0;
+}
