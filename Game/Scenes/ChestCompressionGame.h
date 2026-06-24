d849918eb8473f3eef3de190d28070da4ba25c79
Giuee <eladio121@gmail.com>
2026-06-24 18:24:46 +1000

    ChestCompressionGame
    i think it does stuff

---
+++ Game/Scenes/ChestCompressionGame.h
@@ -0,0 +1,190 @@
+#pragma once
+
+#include "Scene.h"
+#include "Components.h"
+#include "RenderSystem.h"
+#include "PhysicsSystem.h"
+#include "AnimationSystem.h"
+#include "GL/gl.h"
+#include <wiiuse/wpad.h>
+#include <math.h>
+#include "raymath.h"
+#include <algorithm>
+#include <cmath>
+
+
+class ChestCompressionGame : public Scene {
+public:
+	std::shared_ptr<RenderSystem> rendersys;
+	std::shared_ptr<PhysicsSystem> physics;
+	std::shared_ptr<AnimationSystem> animation;
+	Camera3D camera = {};
+
+	//accel
+	float ax, ay, az;
+	//timer
+	float timer = 0.0f;
+	float bpmTimer = 0.0f; 
+	float gameDuration = 60.0f;
+	float refractoryTimer = 0.0f;
+	float lastFlickTime = 0.0f;
+
+	//BPM 
+	float currentBPM = 0.0f
+	float targetBPM = 120.0f; 
+	float bpmWindow = 10f; 
+	float compressionThresh = 0.5f;
+	float refractory = 0.3f; //min seconds between flicks 
+
+	bool isFlicking = false; 
+
+	// Initialise ECS and camera for scene
+	void init() override {
+		// load scene resources/sprites
+		//ResourceId teapotId = world.loadModel("sd:/teapot.obj");
+		//SpriteId testimageId = world.loadSprite("sd:/laser.png");
+		//AnimId testAnimId = world.loadAnim("run", "sd:/scarfy.png", 6, 8);
+
+		timer = gameDuration;
+
+		// Setup camera
+		camera.position = Vector3{ 0.0f, 5.0f, 5.0f };
+		camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
+		camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+		camera.fovy = 90.0f;
+		camera.projection = CAMERA_PERSPECTIVE;
+
+
+		// Setup components and systems
+		world.registerComponent<EngineTransform>();
+		world.registerComponentSerializer<EngineTransform>();
+		world.registerComponent<Renderable>();
+		world.registerComponentSerializer<Renderable>();
+		world.registerComponent<Renderable2D>();
+		world.registerComponentSerializer<Renderable2D>();
+		world.registerComponent<Collider2D>();
+		world.registerComponentSerializer<Collider2D>();
+		world.registerComponent<RigidBody2D>();
+		world.registerComponentSerializer<RigidBody2D>();
+		world.registerComponent<Animator2D>();
+		world.registerComponentSerializer<Animator2D>();
+
+		rendersys = world.registerSystem<RenderSystem>();
+		rendersys->world = &world;
+		rendersys->camera = &camera;
+		physics = world.registerSystem<PhysicsSystem>();
+		physics->world = &world;
+		animation = world.registerSystem<AnimationSystem>();
+		animation->world = &world;
+
+		Signature renderSig;
+		renderSig.set(world.getComponentType<EngineTransform>());
+		world.setSystemSignature<RenderSystem>(renderSig);
+
+		Signature physicsSig;
+		physicsSig.set(world.getComponentType<EngineTransform>());
+		physicsSig.set(world.getComponentType<Collider2D>());
+		physicsSig.set(world.getComponentType<RigidBody2D>());
+		world.setSystemSignature<PhysicsSystem>(physicsSig);
+
+		Signature animationSig;
+		animationSig.set(world.getComponentType<EngineTransform>());
+		animationSig.set(world.getComponentType<Animator2D>());
+		world.setSystemSignature<AnimationSystem>(animationSig);
+
+
+		// Load entities from file
+		FILE* f = fopen("sd:/samplescene.bin", "rb");
+		if (f) 
+		{
+			fclose(f);
+			world.load("sd:/samplescene.bin");
+		}
+		else
+		{
+			// Create new/default entities here
+			return;
+		}
+	}
+
+	void update(float dt, WPADData* data) override {
+		// update inputs, entities, camera etc. here
+		if (data->data_present) {
+			az = data->accel.z;
+			ax = data->accel.x;
+			ay = data->accel.y;
+
+			Vector3 rawAccel = {(float)ax, (float)ay, (float)az};
+			Vector3 normalizedAccel = Vector3Normalize(rawAccel);
+			float normalizedZ = normalizedAccel.z; //normalizing the accel using raylib built in vector3 math 
+
+			bool flicking =  normalizedZ > compressionThresh;
+
+			if (flicking && !isflicking && refractoryTimer >= refractory)
+			{
+				CompressionBPM();
+				refractoryTimer 0.0f;
+			}
+		}
+
+		timer -= dt;
+		refractoryTimer +=dt;
+
+		// update physics system
+		physics->update(dt);
+		physics->updateCollisions(dt, false);
+
+		// update animation system
+		animation->update(dt);
+	}
+
+	Void CompressionBPM()
+	{
+		float clampedBPM = std::clamp(targetBPM, 100.0f, 120.0f);
+		float timeSinceLast = (gameDuration - Timer) - lastFlickTime;
+		lastFlickTime = gameDuration - Timer;
+
+		//ignore flick if too early 
+		if (timeSinceLast <= 0.0f || timeSinceLast <= 0.2f) return 
+
+		currentBPM = 60.0f / timeSinceLast;	 
+	
+		clampedBPM = std::clamp(currentBPM, 100.0f, 120.00f);
+		if (std::abs(currnetBPM - targetBPM) <= bpmWindow 
+		{
+
+		}
+	
+		if else (std:: abs(currentBPM < targetBPM)
+		{
+
+		}
+	
+		else 
+		{
+
+		}
+	}
+	void render(float dt) override {
+		BeginDrawing();
+		ClearBackground(BLACK);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		rendersys->update(dt);
+
+		DrawText(TextFormat("ACCEL: %.0f, %.0f, %.0f", ax, ay, az), 10, 30, 20, WHITE);
+		DrawText(TextFormat("Timer: %.0f", timer), 10, 50, 20, WHITE);
+		DrawText(TextFormat("CurrentBPM: %f", bpmTimer), 10, 70, 20, WHITE);
+		DrawText(TextFormat("TargetBPM: %f", targetBPM), 10, 90, 20, WHITE);
+
+		DrawFPS(10, 10);
+		EndDrawing();
+	}
+
+	void shutdown() override {
+		// save current scene state
+		world.save("sd:/samplescene.bin");
+		// unload scene resources
+		world.unloadAllResources();
+		world.unloadAllSprites();
+	}
+};
